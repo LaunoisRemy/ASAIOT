7506bf0a37f74fe526487dfe3dce99e8839c6c61
Launois.Remy <remy.launois@gmail.com>
2019-10-30 22:18:35 +0100

    mmain change

--- main.py
+++ main.py
@@ -1,50 +1,49 @@
-from utils import buzzer,temphumi,ecranlcd,Button
+from utils import buzzer,temphumi,ecranlcd,thRfid
 import time
 buzzer.init(8) #plug on D8
 temphumi.init(2) #plug on D2
 #ecranlcd.init(0) #plug on I2C-0
 #grovepi.pinMode(buzzer, "OUTPUT")
 
-buttonValidate = Button(3)
-buttonDelete = Button(4)
+#buttonValidate = Button(3)
+#buttonDelete = Button(4)
 
 #Identif(Thread).start()
-
+thread = Identif()
+thread.start()
 ser = serial.Serial('/dev/ttyACM0',115200)
 
 while True:
-	try :
-	#temphumi.readTemp(5) # read temperature each 2 minutes
-	#buzzer.turnOn()
-	#time.sleep(2)
-	#buzzer.turnOff()	
-#OR
-	    temp =  temphumi.showTemp()
-		print(temp)
-		if (temp > 23):
-			print(temp)
-			buzzer.turnOn()
-			
+        try :
+                #temphumi.readTemp(5) # read temperature each 2 minutes
+                #buzzer.turnOn()
+                #time.sleep(2)
+                #buzzer.turnOff()
+                #OR
+                temp =  temphumi.showTemp()
+                print(temp)
+                if (temp > 23):
+                        print(temp)
+                        buzzer.turnOn()
+                        
 			time.sleep(0.06) # 10sec of buzzer
-			buzzer.turnOff()
+                        buzzer.turnOff()
 
 		showTemp = "temp = %.02f C"%temp
-		#ecranlcd.setText(showTemp)
+                #ecranlcd.setText(showTemp)
 
 		id = ser.readLine()
-		print(id)
-		time.sleep(2)
-	# if (Identif.userId == 1){ #un utilisateur s'est connecté
-	# 	#ecrire à l'écran le nom de l'utilisateur + choix
-	# 	#on commence le processus de choix
-	# }
+                print(id)
+                time.sleep(2)
+                if(thread.userId == 1 ) :
+                        print(thread.userId)
+                 
+        except KeyboardInterrupt:
+                grovepi.digitalWrite(buzzer,0)
+                break
 
-except KeyboradInterrupt:
-	grovepi.digitalWrite(buzzer,0)
-	break
-
-except IOError :
-	print ("Error")
-	
+        except IOError :
+                print ("Error")
+        
 
 
